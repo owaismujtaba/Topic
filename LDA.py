6e803d282c0959e00af76361fd78ea499b866992
owaismujtaba <owais.mujtaba123@gmail.com>
2022-09-02 16:57:47 +0200

    l

---
+++ LDA.py
@@ -0,0 +1,8 @@
+import gensim
+from gensim.utils import simple_preprocess
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use'])

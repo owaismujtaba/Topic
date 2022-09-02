b24d3787a84edac01eee62dbcb9737d7558d158c
Owais Mujtaba Khanday <45037843+owaismujtaba@users.noreply.github.com>
2022-09-02 17:12:36 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import pdb
 from data_utils import load_data, drop_meaningless
 from data_utils import text_cleaning, EDA
 
-def get_data()
+def get_data():
     papers = load_data()
     papers = drop_meaningless(papers)
     papers = text_cleaning(papers)

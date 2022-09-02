1f5643b9f78d6a203b88d8b0c63a701264f692bd
Owais Mujtaba Khanday <45037843+owaismujtaba@users.noreply.github.com>
2022-09-02 17:11:06 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,13 @@ import pdb
 from data_utils import load_data, drop_meaningless
 from data_utils import text_cleaning, EDA
 
-
-def generate_wordcloud():
+def get_data()
     papers = load_data()
     papers = drop_meaningless(papers)
     papers = text_cleaning(papers)
+    
+    return papers
+def generate_wordcloud():
+    
+    papers = get_data()
     papers = EDA(papers)
